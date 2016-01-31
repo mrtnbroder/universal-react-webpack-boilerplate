244f673e7958e421962c8d9561ec9a9e06321617
Martin Broder <mrtnbroder@gmail.com>
2016-02-01 00:20:49 +0100

    upgrade package deps + switch to nodemon

--- src/views/about/index.jsx
+++ src/views/about/index.jsx
@@ -4,14 +4,10 @@ import { Link } from 'react-router'
 
 class AboutPage extends Component {
 
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
       <div>
-        <h2>About Page!</h2>
+        <h2>About Page</h2>
         <Link to='/'>Home</Link>
       </div>
     )
