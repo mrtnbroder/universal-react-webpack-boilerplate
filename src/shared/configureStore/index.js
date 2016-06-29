8d54d38a7dd46326bdaa239edfaae52f05916b14
Martin Broder <mrtnbroder@gmail.com>
2016-06-29 09:50:28 +0200

    re-enable hot-reload

--- src/shared/configureStore/index.js
+++ src/shared/configureStore/index.js
@@ -10,13 +10,13 @@ export default (reducers = {}) => (initalState = {}) => {
     __BROWSER__ ? window.devToolsExtension ? window.devToolsExtension() : (f) => f : (f) => f
   )(createStore)(rootReducer, initalState)
 
-  // if (module.hot)
-  //   // Enable Webpack hot module replacement for reducers
-  //   module.hot.accept('../reducers', () => {
-  //     const nextReducer = require('../reducers')
-  //
-  //     store.replaceReducer(nextReducer)
-  //   })
+  if (module.hot)
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../modules', () => {
+      const nextReducer = require('../modules')
+
+      store.replaceReducer(nextReducer)
+    })
 
   return store
 }
