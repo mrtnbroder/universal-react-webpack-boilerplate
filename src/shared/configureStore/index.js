87b59b831604614e991d543fb4102c8b59e0655f
Martin Broder <mrtnbroder@gmail.com>
2016-09-06 11:52:08 +0200

    always require reducers from the user

--- src/shared/configureStore/index.js
+++ src/shared/configureStore/index.js
@@ -2,7 +2,7 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
 
-export default (reducers = {}) => (initalState = {}) => {
+export default (reducers) => (initalState = {}) => {
   const rootReducer = combineReducers(reducers)
   const middleware = [promiseMiddleware()]
   const store = compose(
