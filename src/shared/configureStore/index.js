68e1245b9ef06a22bb683c493692e36e367b992a
mrtnbroder <mrtnbroder@gmail.com>
2016-11-30 01:38:15 +0100

    feat(configureStore): add thunk middleware

--- src/shared/configureStore/index.js
+++ src/shared/configureStore/index.js
@@ -1,14 +1,14 @@
 
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
+import thunk from 'redux-thunk'
 
-export default (reducers) => (initalState = {}) => {
+export default (reducers = {}) => (initalState = {}) => {
   const rootReducer = combineReducers(reducers)
-  const middleware = [promiseMiddleware()]
-  const store = compose(
+  const middleware = [promiseMiddleware(), thunk]
+
+  return compose(
     applyMiddleware(...middleware),
     __BROWSER__ ? window.devToolsExtension ? window.devToolsExtension() : (f) => f : (f) => f
   )(createStore)(rootReducer, initalState)
-
-  return store
 }
